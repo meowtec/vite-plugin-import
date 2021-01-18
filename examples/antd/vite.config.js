1f9a23c6af04db17d4bc063ac13d9a211a254699
meowtec <bertonzh@gmail.com>
2021-01-18 20:09:45 +0800

    chore: update example

--- examples/antd/vite.config.js
+++ examples/antd/vite.config.js
@@ -1,50 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // @ts-check
 import reactPlugin from 'vite-plugin-react';
-import createImportPlugin from 'vite-plugin-import';
+import importPlugin from 'vite-plugin-import';
 
 /**
  * @type { import('vite').UserConfig }
  */
 const config = {
-  jsx: 'react',
   plugins: [
     reactPlugin,
-    createImportPlugin([
-      {
-        libraryName: '@byte-design/ui',
-        libraryDirectory: 'es/components',
-        transformToDefaultImport: false,
-        style: true,
-      },
-      {
-        libraryName: '@byte-design/form',
-        libraryDirectory: 'es/fields',
-        camel2DashComponentName: false,
-        transformToDefaultImport: false,
-        style: false,
-      },
+    importPlugin([
+      { "libraryName": "antd", style: "css" },
     ]),
   ],
-  resolvers: [
-    {
-      alias(path) {
-        if (path.startsWith('@/')) {
-          return path.replace('@/', '/');
-        }
-
-        return null;
-      },
-    },
-  ],
-  optimizeDeps: {
-    include: ['semver/functions/valid'],
-  },
-  base: 'https://sf3-scmcdn-tos.pstatp.com/goofy/sev-platform',
-  proxy: {
-    '/api': 'http://localhost:8899/',
-    '/sso': 'http://localhost:8899/',
-  },
 };
 
 export default config;
