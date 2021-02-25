2c18062783fa317236036bbc72725c1dd46115a4
yangxu <yangxu3@tal.com>
2021-02-26 00:38:17 +0800

    feat: added onlyBuild option

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,49 @@
 import { Plugin } from 'vite';
 import { transformAsync } from '@babel/core';
 
-export type ImportPluginOptions = Array<{
+type BabelImportPluginOptions = Array<{
   [key: string]: any;
   libraryName: string;
   libraryDirectory?: string;
   camel2DashComponentName?: boolean;
 }>;
 
+export type ImportPluginOptions = BabelImportPluginOptions | {
+  onlyBuild?: boolean;
+  babelImportPluginOptions: BabelImportPluginOptions;
+};
+
 export default (options: ImportPluginOptions) => {
+  let babelImportPluginOptions: BabelImportPluginOptions = [];
+  let onlyBuild = true;
+  if (Array.isArray(options)) {
+    babelImportPluginOptions = options;
+  } else {
+    babelImportPluginOptions = options.babelImportPluginOptions;
+    onlyBuild = options.onlyBuild === false ? false : true;
+  }
+  
   /**
    * fast check and pass by code that does not contains libraryName
    */
-  const codeIncludesLibraryName = (code: string) => !options.every(({ libraryName }) => !new RegExp(`('${libraryName}')|("${libraryName}")`).test(code));
+  const codeIncludesLibraryName = (code: string) => !babelImportPluginOptions.every(({ libraryName }) => !new RegExp(`('${libraryName}')|("${libraryName}")`).test(code));
+
+  let isBuild = false;
 
   const plugin: Plugin = {
     name: 'modular-import',
 
+    configResolved(config) {
+      isBuild = config.command === 'build' || config.isProduction;
+    },
+
     async transform(src) {
-      if (!codeIncludesLibraryName(src)) {
+      if ((onlyBuild && !isBuild) || !codeIncludesLibraryName(src)) {
         return undefined;
       }
 
       const result = await transformAsync(src, {
-        plugins: options.map((mod) => ['import', mod, `import-${mod.libraryDirectory}`]),
+        plugins: babelImportPluginOptions.map((mod) => ['import', mod, `import-${mod.libraryDirectory}`]),
       });
 
       return result?.code;
